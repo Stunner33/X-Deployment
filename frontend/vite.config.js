5c648dc4b0b2d66a2146973d0fa89300ad59dc8d
Stunner33 <stunnercadrick@gmail.com>
2025-04-06 21:03:18 +0530

    Initialize

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,14 +1,15 @@
-import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 3000,
+    host: "0.0.0.0",
+    port: 5173,
     proxy: {
       "/api": {
-        target: "http://localhost:5000",
+        target: "http://backend:5000",
         changeOrigin: true,
       },
     },
